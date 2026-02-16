5b9ed7b6812cda12b0c3a20e26ed7715b78729f1
cedric alinc <72914047+cedricalinc@users.noreply.github.com>
2026-02-16 11:12:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,55 +6,63 @@ const recs = [
         n: "Florent VALLÉE", 
         r: "Directeur de l'urgence et des opérations - CRf", 
         t: `Cedric est un Chef de projet engagé, à l'<strong>écoute des besoins</strong> des utilisateurs finaux.<br><br> Son <strong>dynamisme</strong>, sa <strong>créativité </strong>et son <strong>engagement </strong>font sa difference`, 
-        img: "https://media.licdn.com/dms/image/v2/C4D03AQHkBheE0s6K2g/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1568715661165?e=1770854400&v=beta&t=ZcyExSNEKq8U2FwhyWn22YjUUN8Y_UoE6SLzhax6hGo", 
+        img: "https://media.licdn.com/dms/image/v2/C4D03AQHkBheE0s6K2g/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1568715661165?e=1772668800&v=beta&t=uyT3-gDRgUUENY0OX3IPIxUkKt1Qj3LeV1IIpC_kC_E", 
         lien: "https://www.linkedin.com/in/florent-vall%C3%A9e-b13a8b116",
         tags: ["Product", "User-Centric", "SoftSkills"]
+    },
+    { 	
+        n: "Nelly PEREZ", 
+        r: "Directrice adjointe - Programme Malin", 
+        t: `Cédric est <strong>très professionnel et impliqué</strong> dans tout ce qu'il fait. Il apporte une <strong>vision à la fois stratégique et pragmatique toujours très pertinente et adaptée</strong> à la situation ou aux personnes qu'il accompagne. Il <strong>mène toujours à bien ses projets sans abandonner mais en s'adaptant aux circonstances et aux personnes</strong>, avec un supplément <strong>créativité très appréciable</strong>. C'est un plaisir de travailler avec lui au quotidien. `, 
+        img: "https://media.licdn.com/dms/image/v2/D5603AQESfOVGie0-tA/profile-displayphoto-scale_100_100/B56Zd80fYxG0Ak-/0/1750145825144?e=1772668800&v=beta&t=rVP7bx-y6VhbeekTTyj6KtiYqtkirLLutfwDi-Y9n5Y", 
+        lien: "https://www.linkedin.com/in/nelly-perez-544515a6",
+        tags: ["Créativité", "UX Strategy", "SoftSkills"]
     },
     { 	
         n: "Emma HEDEF", 
         r: "Directrice Digital et Marketing - Programme Malin", 
         t: `Je tiens à partager un retour très positif sur l’accompagnement de Cédric en tant que Product Manager auprès de notre association Programme Malin. <br><br>Il nous a aidés à concevoir un espace de travail centralisé sur Notion, inspiré de la méthode Bullet Journal, pour mieux <strong>aligner la vision</strong>, <strong>clarifier </strong>les rôles, <strong>prioriser </strong>efficacement et <strong>fluidifier</strong> la collaboration entre plusieurs intervenants. <br><br>Au-delà des outils, j’ai particulièrement apprécié sa <strong>pédagogie</strong>, sa <strong>patience</strong> et la <strong>pertinence de ses recommandations</strong>.<br><br> Il a su <strong>structurer des besoins complexes sans les alourdir</strong>, et <strong>accompagner les équipes</strong> vers plus de clarté et d’autonomie dès les premières semaines. <br><br>Un <strong>accompagnement à la fois structurant, pragmatique et humain</strong>, que je recommande sans hésiter.`, 
-        img: "https://media.licdn.com/dms/image/v2/D4E03AQEKgOUv8RsPew/profile-displayphoto-scale_400_400/B4EZv6AUrIGcAg-/0/1769425949036?e=1770854400&v=beta&t=G3xFOP_6MQ3Kv35Td2IykMHZVyVqlezuFlnIn3qQl5w", 
+        img: "https://media.licdn.com/dms/image/v2/D4E03AQEKgOUv8RsPew/profile-displayphoto-scale_100_100/B4EZv6AUrIGcAc-/0/1769425949036?e=1772668800&v=beta&t=0X8Tl1N2FVs5dzDlQFOs6jbTxoS5su2VIBV3MO5Hju8", 
         lien: "https://www.linkedin.com/in/emma-hedef",
         tags: ["Workflow", "Product", "User-Centric", "Pédagogie", "SoftSkills"] 
-    },
+    },    
     { 
         n: "Bertrand OGIER", 
         r: "Directeur pédagogique | Formations à impact | ESS - Innovation - Inclusion", 
-        t: `Cédric est un <strong>professionnel compétent et engagé</strong>. Doué d'une <strong>capacité d'analyse et de structuration des projets</strong>, il est aussi source d'<strong>avis pertinents</strong>, que cela soit d'un point de vue <strong>technique ou stratégique</strong>. Enfin Cédric est un collègue très sympathique et qui fait preuve d'un <strong>esprit d'équipe affirmé</strong>.`, 
-        img: "https://media.licdn.com/dms/image/v2/D4E35AQHO3d3JZ5SUgw/profile-framedphoto-shrink_100_100/B4EZrfwx4MKMAo-/0/1764690698247?e=1770674400&v=beta&t=odeH_ileGyjHtB1fL9HxH7288HS2qdlBmy63WX9-xj8", 
+        t: "Cédric est un <strong>professionnel compétent et engagé</strong>. Doué d'une <strong>capacité d'analyse et de structuration des projets</strong>, il est aussi source d'<strong>avis pertinents, que cela soit d'un point de vue technique ou stratégique</strong>. Enfin Cédric est un collègue très <strong>sympathique et qui fait preuve d'un esprit d'équipe affirmé</strong>.", 
+        img: "https://media.licdn.com/dms/image/v2/D4E35AQHO3d3JZ5SUgw/profile-framedphoto-shrink_100_100/B4EZrfwx4MKMAo-/0/1764690698247?e=1771840800&v=beta&t=oYsoNcNaQkFFcAqX5lmAo5QVMWE47vZdIWwLv5bZ70w", 
         lien: "https://www.linkedin.com/in/bertrand-ogier-a2874716",
-        tags: ["Analyse", "Organisation", "SoftSkills","Relationnel"]
+        tags: ["Organisation", "Réactivité", "Product"]
     },
     { 
         n: "Saïname ALIBAY", 
         r: "Responsable du service Animation, Communication et Placement entreprises - CFA Numia / EFREI", 
         t: `J’ai eu le plaisir de travailler avec Cédric Alinc, un excellent collègue, aussi apprécié pour ses qualités humaines que pour son professionnalisme.<strong>Fiable, impliqué et toujours à l’écoute</strong>, Cédric sait créer un <strong>climat de travail collaboratif</strong> et contribue pleinement à créer un <strong>environnement de travail positif et stimulant</strong>. Cédric est une personne sur qui l’on peut compter et avec qui il est réellement agréable de travailler.Je le recommande sans hésitation.`, 
-        img: "https://media.licdn.com/dms/image/v2/D4E03AQGVVGfrdlnBZw/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1666281302502?e=1771459200&v=beta&t=ZFNUMJTHwn2-q6ZQCv-1mmhqjuTCRCMkVp5frXbZfuQ", 
+        img: "https://media.licdn.com/dms/image/v2/D4E03AQGVVGfrdlnBZw/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1666281302502?e=1772668800&v=beta&t=DMGvFWHyeLqfJx2G72_cU0UxT1772pOTgpX_3bt2loU", 
         lien: "https://www.linkedin.com/in/sa%C3%AFname-alibay",
         tags: ["SoftSkills", "Relationnel", "Product"]
-    },
-    { 
-        n: "Mahé BANDEIRA DE VIVAR", 
-        r: "Chef de projet - iPorta", 
-        t: "Cédric est quelqu'un de très <strong>organisé</strong>, très <strong>réactif </strong>et <strong>vif d'esprit</strong>. <br><br>J'ai apprécié sa <strong>qualité de travail</strong>.", 
-        img: "https://media.licdn.com/dms/image/v2/D4E03AQF-qGvOqZCa6Q/profile-displayphoto-shrink_100_100/B4EZb80nHNHIAU-/0/1747998373569?e=1770249600&v=beta&t=x1E33hJYt9bW8ZleLlg669bMmXwwsKRA7oOVoPIhq78", 
-        lien: "https://www.linkedin.com/in/mahe-de-vivar/",
-        tags: ["Organisation", "Réactivité", "Product"]
     },
     { 
         n: "Damya BOUKHEMAL", 
         r: "Product Owner - CFA Numia", 
         t: `J’ai beaucoup apprécié travailler avec Cédric.<br> <br>Son <strong>expérience </strong>et sa <strong>vision </strong>ont apporté une réelle plus value à notre équipe.<br> <br>Au-delà de ses <strong>solides compétences professionnelles</strong>, c’est un manager qui sait les mettre en œuvre avec <strong>efficacité </strong>et surtout les <strong>partager avec son équipe</strong>. <br>J’ai personnellement beaucoup appris à ses côtés, tant sur le plan technique que méthodologique. <br><br>Son <strong>sens du collectif</strong>, sa capacité à <strong>accompagner</strong> et à <strong>faire monter les autres en compétences</strong> font de lui un <strong>professionnel fiable et engagé</strong>.`, 
-        img: "https://media.licdn.com/dms/image/v2/D4E03AQGSetwnwBe-9w/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1686747440629?e=1770249600&v=beta&t=emrdqqW3z5ceMDb-_t8eCcywMYJuZQZppFUs67SBb7U", 
+        img: "https://media.licdn.com/dms/image/v2/D4E03AQGSetwnwBe-9w/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1686747440629?e=1772668800&v=beta&t=ivsxXbHORJ4EhNYYD727LjJgqSOzmX2xcz0uuqJ0OIU", 
         lien: "https://www.linkedin.com/in/damya-boukhemal/",
         tags: ["Management", "Mentoring", "Product"] 
+    },
+    { 
+        n: "Mahé BANDEIRA DE VIVAR", 
+        r: "Chef de projet - iPorta", 
+        t: "Cédric est quelqu'un de très <strong>organisé</strong>, très <strong>réactif </strong>et <strong>vif d'esprit</strong>. <br><br>J'ai apprécié sa <strong>qualité de travail</strong>.", 
+        img: "https://media.licdn.com/dms/image/v2/D4E03AQF-qGvOqZCa6Q/profile-displayphoto-shrink_100_100/B4EZb80nHNHIAU-/0/1747998373569?e=1772668800&v=beta&t=tIDJd7-TsHLG_nflxDNCebCEuzK6WIiGjNN6ETus-io", 
+        lien: "https://www.linkedin.com/in/mahe-de-vivar/",
+        tags: ["Organisation", "Réactivité", "Product"]
     },
     { 	
         n: "Ludovic LEVY", 
         r: "Pilote Google Workspace Accompagnement et Gouvernance - CRf", 
         t: `J'ai eu le plaisir de collaborer avec Cédric pendant 5 ans et c’est une <strong>personne sur qui l’on peut compter</strong> en toutes circonstances. <br><br>Au-delà de ses compétences de Product Manager, j'ai particulièrement apprécié la <strong>solidité de ses engagements</strong> et sa <strong>loyauté</strong> exemplaire.<br><br> Cédric est un collaborateur toujours <strong>jovial</strong>, qui apporte une <strong>énergie positive</strong> au sein d'une équipe. <br><br>Je le recommande vivement pour son <strong>professionnalisme </strong>et ses <strong>qualités humaines rares</strong>.`, 
-        img: "https://media.licdn.com/dms/image/v2/D4E03AQHpM0Tmo9W20A/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1700547120552?e=1770249600&v=beta&t=bOkdL8GlSc9TsFHkSQTguflZACma6f8c9LhnoR5x2Vk", 
+        img: "https://media.licdn.com/dms/image/v2/D4E03AQHpM0Tmo9W20A/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1700547120552?e=1772668800&v=beta&t=_hyr7-xtsseVqwZ_juCGtaW6YTFear-mjt62ytLqC9w", 
         lien: "https://www.linkedin.com/in/ludovic-levy-94071a55",
         tags: ["Product", "SoftSkills", "Relationnel"]
     },
@@ -62,11 +70,20 @@ const recs = [
         n: "Marlène BIARD", 
         r: "#Slasheuse : Communication engagée | Coaching professionnel | Bilans de compétences | Animation de conférences | Evénementiel | Formation soft skills", 
         t: `J’ai eu le plaisir de travailler avec Cédric à la Croix-Rouge, où il occupait le rôle de chef de projet en maîtrise d’ouvrage informatique. <br><br>J’ai particulièrement apprécié sa grande <strong>réactivité</strong> et sa capacité à apporter des <strong>réponses concrètes et pragmatiques</strong> aux problématiques rencontrées.<br><br> <strong>Toujours à l’écoute des besoins</strong> des utilisateurs et des parties prenantes, Cédric sait <strong>transformer des enjeux</strong> parfois complexes en <strong>solutions simples et efficaces</strong>, avec un objectif constant : <strong>améliorer et fluidifier l’expérience client</strong>. <br><br>Son <strong>sens du service, sa rigueur et son engagement </strong>ont été de réels atouts pour la réussite des projets menés ensemble.`, 
-        img: "https://media.licdn.com/dms/image/v2/D4E03AQEA5toIIcQxvg/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1732107701825?e=1770854400&v=beta&t=ciIw_cRqMDi_K1hRox2TRdbx352Db1hmygRBlT7w2kU", 
+        img: "https://media.licdn.com/dms/image/v2/D4E03AQEA5toIIcQxvg/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1732107701825?e=1772668800&v=beta&t=6zPOUrk8mqT3DJQ5GhrQTOBddBWONC7zCCYHnHDNNxY", 
         lien: "https://www.linkedin.com/in/marlene-biard-53860742",
         tags: ["Organisation", "UX Strategy", "SoftSkills"]
+    },
+    { 
+        n: "Amélie ROBIN", 
+        r: "Chef de projet - Lyon Métropole Habitat", 
+        t: "J’ai eu le plaisir de travailler avec Cédric il y a quelques années : <strong>fiable et posé</strong> en toutes circonstances pour <strong>gérer des projets et leurs aléas</strong>, c’est un <strong>atout dans l’équipe, qui sait écouter et dialoguer</strong> pour faire avancer le projet.", 
+        img: "https://media.licdn.com/dms/image/v2/C4D03AQEz7eQ7cJXnQQ/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1618425149563?e=1772668800&v=beta&t=c1jw4gDtGnYVvQgIpPiJbztOQEbPkbp__uvDK3hHu9U", 
+        lien: "https://www.linkedin.com/in/am%C3%A9lie-robin-11ab33b8",
+        tags: ["SoftSkills", "Réactivité", "Organisation","Product"]
     }
 ];
+
 
 
 const exps = {
